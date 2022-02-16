7e888590f3760935d9ba8be5b0c8450568b5a7f7
Vitalii Shkibtan <vetalguru@gmail.com>
2022-02-16 19:28:01 +0200

    Create json node like part of document

--- libvbf/src/json_parser.cpp
+++ libvbf/src/json_parser.cpp
@@ -7,46 +7,214 @@
 
 namespace json {
 
-JSONDocument::JSONDocument() {}
-
-JSONDocument::~JSONDocument() {}
-
-void JSONDocument::Parse(const std::string &json_string) noexcept {
-  try {
-    root_ = ParseJsonString(json_string);
-  } catch (...) {
-    root_ = nullptr;
-  }
+bool JSONNode::IsValid() const noexcept {
+  DataType type = Type();
+  return type != DataType::NULL_TYPE;
 }
 
-bool JSONDocument::IsValid() const { return root_ != nullptr; }
+JSONNode::DataType JSONNode::Type() const noexcept {
+  DataType result{DataType::NULL_TYPE};
+
+  switch (node_.kind()) {
+  case boost::json::kind::object: {
+    result = DataType::OBJECT_TYPE;
+    break;
+  }
+  case boost::json::kind::array: {
+    result = DataType::ARRAY_TYPE;
+    break;
+  }
+  case boost::json::kind::string: {
+    result = DataType::STRING_TYPE;
+    break;
+  }
+  case boost::json::kind::uint64: {
+    result = DataType::UINT64_TYPE;
+    break;
+  }
+  case boost::json::kind::int64: {
+    result = DataType::INT64_TYPE;
+    break;
+  }
+  case boost::json::kind::double_: {
+    result = DataType::DOUBLE_TYPE;
+    break;
+  }
+  case boost::json::kind::bool_: {
+    result = DataType::BOOL_TYPE;
+    break;
+  }
+  case boost::json::kind::null: {
+    result = DataType::NULL_TYPE;
+    break;
+  }
+  default: {
+    break;
+  }
+  }
+
+  return result;
+}
+
+bool JSONNode::IsObject() const noexcept { return node_.is_object(); }
+
+bool JSONNode::IsArray() const noexcept { return node_.is_array(); }
+
+bool JSONNode::IsString() const noexcept { return node_.if_string(); }
+
+bool JSONNode::IsUint64() const noexcept { return node_.is_uint64(); }
+
+bool JSONNode::IsInt64() const noexcept { return node_.is_int64(); }
+
+bool JSONNode::IsDouble() const noexcept { return node_.is_double(); }
+
+bool JSONNode::IsBool() const noexcept { return node_.is_bool(); }
+
+bool JSONNode::IsNull() const noexcept { return node_.is_null(); }
+
+bool JSONNode::GetObjectNodes(std::vector<std::pair<std::string, JSONNode>>
+                                  &object_value) const noexcept {
+  bool result{true};
+  object_value.clear();
+
+  if (IsObject()) {
+    auto const &obj = node_.get_object();
+    if (!obj.empty()) {
+      auto it = obj.begin();
+      for (;;) {
+        std::pair<std::string, boost::json::value> current(
+            boost::json::serialize(it->key()), it->value());
+        object_value.emplace_back(current);
+        if (++it == obj.end()) {
+          break;
+        }
+      }
+    }
+  } else {
+    result = false;
+  }
+
+  return result;
+}
+
+bool JSONNode::GetArrayNodes(
+    std::vector<JSONNode> &array_value) const noexcept {
+  bool result{true};
+  array_value.clear();
+
+  if (IsArray()) {
+    auto const &array = node_.get_array();
+    if (!array.empty()) {
+      for (auto it = array.begin();;) {
+        array_value.emplace_back(*it);
+        if (++it == array.end()) {
+          break;
+        }
+      }
+    }
+  } else {
+    result = false;
+  }
+
+  return result;
+}
+
+bool JSONNode::GetStringValue(std::string &string_value) const noexcept {
+  bool result{true};
+  string_value.clear();
+
+  if (IsString()) {
+    string_value = boost::json::serialize(node_.get_string());
+  } else {
+    result = false;
+  }
+
+  return result;
+}
+
+bool JSONNode::GetUInt64Value(uint64_t &uint64_value) const noexcept {
+  bool result{true};
+
+  if (IsUint64()) {
+    uint64_value = node_.get_uint64();
+  } else {
+    result = false;
+  }
+
+  return result;
+}
+
+bool JSONNode::GetInt64Value(int64_t &int64_value) const noexcept {
+  bool result{true};
+
+  if (IsInt64()) {
+    int64_value = node_.get_int64();
+  } else {
+    result = false;
+  }
+
+  return result;
+}
+
+bool JSONNode::GetDoubleValue(double &doulbe_value) const noexcept {
+  bool result{true};
+
+  if (IsDouble()) {
+    doulbe_value = node_.get_double();
+  } else {
+    result = false;
+  }
+
+  return result;
+}
+
+bool JSONNode::GetBoolValue(bool &bool_value) const noexcept {
+  bool result{true};
+
+  if (IsBool()) {
+    bool_value = node_.get_bool();
+  } else {
+    result = false;
+  }
+
+  return result;
+}
+
+void JSONDocument::Parse(const std::string &json_string) noexcept {
+  root_ = ParseJsonString(json_string);
+}
+
+bool JSONDocument::IsValid() const { return root_.IsValid(); }
 
 void JSONDocument::Print() const {
   if (IsValid())
     PrintJson(std::cout, root_);
 }
 
-boost::json::value
-JSONDocument::ParseJsonString(const std::string &json_string) {
-  boost::json::value result = nullptr;
+JSONNode
+JSONDocument::ParseJsonString(const std::string &json_string) noexcept {
+  JSONNode result;
 
-  try {
-    boost::json::stream_parser parser;
-    parser.write(json_string);
+  if (json_string.empty())
+    return result;
+
+  boost::json::error_code ec;
+  boost::json::parser parser;
+
+  parser.write(json_string, ec);
+  if (!ec) {
     result = parser.release();
-  } catch (...) {
-    result = nullptr;
   }
 
   return result;
 }
 
-void JSONDocument::PrintJson(std::ostream &os, boost::json::value const &node,
+void JSONDocument::PrintJson(std::ostream &os, JSONNode const &node,
                              std::string indent) const {
-  switch (node.kind()) {
-  case boost::json::kind::object: {
-    std::vector<std::pair<std::string, boost::json::value>> object_value;
-    GetObjectNodes(node, object_value);
+  switch (node.Type()) {
+  case JSONNode::DataType::OBJECT_TYPE: {
+    std::vector<std::pair<std::string, JSONNode>> object_value;
+    node.GetObjectNodes(object_value);
 
     os << "{\n";
     indent.append(4, ' ');
@@ -65,9 +233,9 @@ void JSONDocument::PrintJson(std::ostream &os, boost::json::value const &node,
 
     break;
   }
-  case boost::json::kind::array: {
-    std::vector<boost::json::value> array_value;
-    GetArrayNodes(node, array_value);
+  case JSONNode::DataType::ARRAY_TYPE: {
+    std::vector<JSONNode> array_value;
+    node.GetArrayNodes(array_value);
 
     os << "[\n";
     indent.append(4, ' ');
@@ -87,33 +255,33 @@ void JSONDocument::PrintJson(std::ostream &os, boost::json::value const &node,
 
     break;
   }
-  case boost::json::kind::string: {
+  case JSONNode::DataType::STRING_TYPE: {
     std::string value;
-    GetStringValue(node, value);
+    node.GetStringValue(value);
     os << value;
     break;
   }
-  case boost::json::kind::uint64: {
+  case JSONNode::DataType::UINT64_TYPE: {
     uint64_t value{};
-    GetUInt64Value(node, value);
+    node.GetUInt64Value(value);
     os << value;
     break;
   }
-  case boost::json::kind::int64: {
+  case JSONNode::DataType::INT64_TYPE: {
     int64_t value{};
-    GetInt64Value(node, value);
+    node.GetInt64Value(value);
     os << value;
     break;
   }
-  case boost::json::kind::double_: {
+  case JSONNode::DataType::DOUBLE_TYPE: {
     double value{};
-    GetDoubleValue(node, value);
+    node.GetDoubleValue(value);
     os << value;
     break;
   }
-  case boost::json::kind::bool_: {
+  case JSONNode::DataType::BOOL_TYPE: {
     bool value{true};
-    GetBoolValue(node, value);
+    node.GetBoolValue(value);
     if (value) {
       os << "true";
     } else {
@@ -121,7 +289,7 @@ void JSONDocument::PrintJson(std::ostream &os, boost::json::value const &node,
     }
     break;
   }
-  case boost::json::kind::null: {
+  case JSONNode::DataType::NULL_TYPE: {
     os << "null";
     break;
   }
@@ -132,196 +300,6 @@ void JSONDocument::PrintJson(std::ostream &os, boost::json::value const &node,
   if (indent.empty()) {
     os << "\n";
   }
-}
-
-bool JSONDocument::IsBool(boost::json::value const &node) const noexcept {
-  return node.is_bool();
-}
-
-bool JSONDocument::GetBoolValue(boost::json::value const &node,
-                                bool &bool_value) const noexcept {
-  bool result{true};
-
-  if (IsBool(node)) {
-    bool_value = node.get_bool();
-  } else {
-    result = false;
-  }
-
-  return result;
-}
-
-bool JSONDocument::IsDouble(boost::json::value const &node) const noexcept {
-  return node.is_double();
-}
-
-bool JSONDocument::GetDoubleValue(boost::json::value const &node,
-                                  double &doulbe_value) const noexcept {
-  bool result{true};
-
-  if (IsDouble(node)) {
-    doulbe_value = node.get_double();
-  } else {
-    result = false;
-  }
-
-  return result;
-}
-
-bool JSONDocument::IsInt64(boost::json::value const &node) const noexcept {
-  return node.is_int64();
-}
-
-bool JSONDocument::GetInt64Value(boost::json::value const &node,
-                                 int64_t &int64_value) const noexcept {
-  bool result{true};
-
-  if (IsInt64(node)) {
-    int64_value = node.get_int64();
-  } else {
-    result = false;
-  }
-
-  return result;
-}
-
-bool JSONDocument::IsUInt64(boost::json::value const &node) const noexcept {
-  return node.is_uint64();
-}
-
-bool JSONDocument::GetUInt64Value(boost::json::value const &node,
-                                  uint64_t &uint64_value) const noexcept {
-  bool result{true};
-
-  if (IsUInt64(node)) {
-    uint64_value = node.get_uint64();
-  } else {
-    result = false;
-  }
-
-  return result;
-}
-
-bool JSONDocument::IsString(boost::json::value const &node) const noexcept {
-  return node.if_string();
-}
-
-bool JSONDocument::GetStringValue(boost::json::value const &node,
-                                  std::string &string_value) const noexcept {
-  bool result{true};
-  string_value.clear();
-
-  if (IsString(node)) {
-    string_value = boost::json::serialize(node.get_string());
-  } else {
-    result = false;
-  }
-
-  return result;
-}
-
-bool JSONDocument::IsArray(boost::json::value const &node) const noexcept {
-  return node.is_array();
-}
-
-bool JSONDocument::GetArrayNodes(
-    boost::json::value const &node,
-    std::vector<boost::json::value> &array_value) const noexcept {
-  bool result{true};
-  array_value.clear();
-
-  if (IsArray(node)) {
-    auto const &array = node.get_array();
-    if (!array.empty()) {
-      for (auto it = array.begin();;) {
-        array_value.push_back(*it);
-        if (++it == array.end()) {
-          break;
-        }
-      }
-    }
-  } else {
-    result = false;
-  }
-
-  return result;
-}
-
-bool JSONDocument::IsObject(boost::json::value const &node) const noexcept {
-  return node.is_object();
-}
-
-bool JSONDocument::GetObjectNodes(
-    boost::json::value const &node,
-    std::vector<std::pair<std::string, boost::json::value>> &object_value)
-    const noexcept {
-  bool result{true};
-  object_value.clear();
-
-  if (IsObject(node)) {
-    auto const &obj = node.get_object();
-    if (!obj.empty()) {
-      auto it = obj.begin();
-      for (;;) {
-        std::pair<std::string, boost::json::value> current(
-            boost::json::serialize(it->key()), it->value());
-        object_value.push_back(current);
-        if (++it == obj.end()) {
-          break;
-        }
-      }
-    }
-  } else {
-    result = true;
-  }
-
-  return result;
-}
-
-bool JSONDocument::IsNull(boost::json::value const &node) const noexcept {
-  return node.is_null();
-}
-
-JSONDocument::DataType
-JSONDocument::NodeType(boost::json::value const &node) const noexcept {
-  JSONDocument::DataType result{JSONDocument::DataType::UNKNOWN_TYPE};
-
-  switch (node.kind()) {
-  case boost::json::kind::object: {
-    result = JSONDocument::DataType::OBJECT_TYPE;
-    break;
-  }
-  case boost::json::kind::array: {
-    result = JSONDocument::DataType::ARRAY_TYPE;
-    break;
-  }
-  case boost::json::kind::string: {
-    result = JSONDocument::DataType::STRING_TYPE;
-    break;
-  }
-  case boost::json::kind::uint64: {
-    result = JSONDocument::DataType::UINT64_TYPE;
-    break;
-  }
-  case boost::json::kind::int64: {
-    result = JSONDocument::DataType::INT64_TYPE;
-    break;
-  }
-  case boost::json::kind::double_: {
-    result = JSONDocument::DataType::DOUBLE_TYPE;
-    break;
-  }
-  case boost::json::kind::bool_: {
-    result = JSONDocument::DataType::BOOL_TYPE;
-    break;
-  }
-  case boost::json::kind::null: {
-    result = JSONDocument::DataType::NULL_TYPE;
-    break;
-  }
-  }
-
-  return result;
 }
 
 } // namespace json
